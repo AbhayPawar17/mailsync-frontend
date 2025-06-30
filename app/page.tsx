ebaa990f9f290b715afa96ccfcfe17aeacdeeda0
Abhay17Pawar <abhaypawar924@gmail.com>
2025-06-30 12:00:28 +0530

    changed the route name

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
-"use client"
+"use client";
 
-import { usePathname } from "next/navigation"
-import AIEmailLandingPage from "./landing-page/page"
+import { usePathname } from "next/navigation";
+import AIEmailLandingPage from "./landing-page/page";
 import SyncMail from "./sync-mail/page";
 import Dashboard from "./dashboard/page";
 
-export default function Home() {
-      const pathname = usePathname();
-  <>
+export default function App() {
+  const pathname = usePathname();
 
-      {pathname === "/" && (
-      <AIEmailLandingPage/> )}
-        {pathname === "/sync-mail" && (
-      <SyncMail/>
-      )}
-          {pathname === "/dashboard" && ( 
-      <Dashboard/>)}
-  </>
+  return (
+    <>
+      {pathname === "/" && <AIEmailLandingPage />}
+      {pathname === "/sync-mail" && <SyncMail />}
+      {pathname === "/dashboard" && <Dashboard />}
+    </>
+  );
 }
